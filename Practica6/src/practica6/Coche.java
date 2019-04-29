3c49160e3c3b9a669af03016cd6fbcee3bbad921
mjesusoj <m.jesusojedasalvador@gmail.com>
2019-04-29 12:41:19 +0200

    Aplicados factores de refactorización a los métodos implementados como el Modelo A, B Y C además de renombrar variables #2 #3

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -1,40 +1,56 @@
 package practica6;
 
 public class Coche {
+	private static final String coche = "El coche con matricula ";
 	String matricula;
-	String atrib;
+	String tipo;
 	String modelo;
 	String fabricante;
 
 	public Coche() {
 		matricula = "";
-		atrib = "";
+		tipo = "";
 		modelo = "";
 		fabricante = "";
 	}
 
-	public Coche(String m, String c, String mo, String f) {
+	public Coche(String m, String t, String mo, String f) {
 		matricula = m;
-		atrib = c;
+		tipo = t;
 		modelo = mo;
 		fabricante = f;
 	}
 
-	public String metodo1() {
+	public String metodoinformativo() {
 		String resultado = "";
-		if (atrib == "Gasolina") {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoA(modelo, fabricante);
-		} else if (atrib == "Diesel") {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoB(modelo, fabricante);
-		} else if (atrib == "Híbrido") {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoC(modelo, fabricante);
+		if (tipo == "Gasolina") {
+			resultado += coche + matricula + " ";
+			resultado += metodoGasolina(modelo, fabricante);
+		} else if (tipo == "Diesel") {
+			resultado += coche + matricula + " ";
+			resultado += metodoDiesel(modelo, fabricante);
+		} else if (tipo == "Híbrido") {
+			resultado += coche + matricula + " ";
+			resultado += metodoHibrido(modelo, fabricante);
 		} else {
-			resultado += "El coche con matricula " + matricula + " ";
+			resultado += coche + matricula + " ";
 			resultado += "no dispone de información";
 		}
 		return resultado;
+	}
+
+	private String metodoGasolina(String modelo, String fabricante) {
+		String resultadoGasolina = "es un" + fabricante + modelo + "y gasta 1,337 euros por litro";
+		return resultadoGasolina;
+	}
+
+	private String metodoDiesel(String modelo, String fabricante) {
+		String resultadoDiesel = "es un" + fabricante + modelo + "y gasta 1,052 euros por litro";
+		return resultadoDiesel;
+	}
+
+	private String metodoHibrido(String modelo, String fabricante) {
+		String resultadoHibrido = "es un" + fabricante + modelo + "y no necesita combustible";
+		return resultadoHibrido;
 	}
 }
